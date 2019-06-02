b9be6267faf05984d29264249ba01b85d778ebb5
Onur ONDER <onur.onder@arkas.com>
2019-06-02 05:16:20 +0300

    fixed export

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,5 @@
 declare module "react-infinite-scroll-hook" {
-  type VoidFunction = () => void;
-
-  export function useWindowSize(): { width: number; height: number };
-
-  interface useInfiniteScrollProp {
-    loading: boolean;
-    hasNextPage: boolean;
-    loadMore: VoidFunction;
-    threshold: number;
-    checkInterval: number;
-    scrollContainer: "window" | "parent";
-  }
-
-  export function useInfiniteScroll(): React.MutableRefObject<undefined>;
-
-  export function useInterval(callback: Function, delay: number): void;
+  export function useInfiniteScroll(
+    props: useInfiniteScrollProps
+  ): React.MutableRefObject<undefined>;
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
 export { default as useInfiniteScroll } from "./useInfiniteScroll";
-export { default as useWindowSize } from "./useWindowSize";
-export { default as useInterval } from "./useInterval";
