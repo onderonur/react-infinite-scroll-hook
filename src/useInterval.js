36ff508578e42f97f16dcb568a5def8aacf1ecaa
Onur ONDER <onur.onder@arkas.com>
2020-01-22 00:26:58 +0300

    initial value for ref

--- src/useInterval.js
+++ src/useInterval.js
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from 'react';
 
 function useInterval(callback, delay) {
-  const savedCallback = useRef();
+  const savedCallback = useRef(null);
 
   useEffect(() => {
     savedCallback.current = callback;
