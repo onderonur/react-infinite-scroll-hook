71ef76e066b738996ce5c2b43a8683d83aac406f
onderonur <onuronder.onr@gmail.com>
2024-10-09 23:10:22 +0300

    prettier config improvement with @vercel/style-guide

--- prettier.config.js
+++ prettier.config.js
@@ -1,15 +1,15 @@
+const styleguide = require('@vercel/style-guide/prettier');
+
 /** @type {import("prettier").Config} */
 module.exports = {
-  singleQuote: true,
-  trailingComma: 'all',
-  endOfLine: 'lf',
+  ...styleguide,
   // To skip destructive code actions of `prettier-plugin-organize-imports`,
   // removing unused imports:
   // https://www.npmjs.com/package/prettier-plugin-organize-imports#skip-destructive-code-actions
   organizeImportsSkipDestructiveCodeActions: true,
   plugins: [
+    ...styleguide.plugins,
     'prettier-plugin-organize-imports',
-    'prettier-plugin-packagejson',
     // Should be the last one.
     // https://github.com/tailwindlabs/prettier-plugin-tailwindcss?tab=readme-ov-file#compatibility-with-other-prettier-plugins
     'prettier-plugin-tailwindcss',
