1d3deaf476565db041ae703c090838ed8e5ba0bc
onderonur <onuronder.onr@gmail.com>
2024-12-13 21:41:39 +0300

    minor improvement

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,10 @@
-const styleguide = require('@vercel/style-guide/prettier');
+import styleguide from '@vercel/style-guide/prettier';
 
-/** @type {import("prettier").Config} */
-module.exports = {
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
   ...styleguide,
   // To skip destructive code actions of `prettier-plugin-organize-imports`,
   // removing unused imports:
@@ -15,3 +18,5 @@ module.exports = {
     'prettier-plugin-tailwindcss',
   ],
 };
+
+export default config;
